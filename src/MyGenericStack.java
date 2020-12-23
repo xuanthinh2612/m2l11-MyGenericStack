e5210185ef038a9b438179b85dbbe9626d5c6e86
gio lang du <you@example.com>
2020-12-23 15:05:52 +0700

    bai thuc hanh 1 - Stack

---
+++ src/MyGenericStack.java
@@ -0,0 +1,32 @@
+import java.util.EmptyStackException;
+import java.util.LinkedList;
+
+public class MyGenericStack<T> {
+    private LinkedList<T> stack;
+
+    public MyGenericStack() {
+        stack = new LinkedList();
+    }
+
+    public void push(T e) {
+        stack.addFirst(e);
+    }
+
+    public T pop() {
+        if (stack.isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return stack.removeFirst();
+    }
+
+    public int size() {
+        return stack.size();
+    }
+
+    public boolean isEmpty() {
+        if (stack.size() == 0) {
+            return true;
+        }
+        return false;
+    }
+}
